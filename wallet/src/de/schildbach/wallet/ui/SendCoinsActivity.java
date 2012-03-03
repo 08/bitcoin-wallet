04719ed145de455e385311689f20911f221d3095
Andreas Schildbach <andreas@schildbach.de>
2012-03-03 13:46:01 +0100

    prod: Remove 'test' flavor from version

--- wallet/src/de/schildbach/wallet/ui/SendCoinsActivity.java
+++ wallet/src/de/schildbach/wallet/ui/SendCoinsActivity.java
@@ -35,7 +35,7 @@ import com.google.bitcoin.uri.BitcoinURI;
 import com.google.bitcoin.uri.BitcoinURIParseException;
 
 import de.schildbach.wallet.util.Bluetooth;
-import de.schildbach.wallet_test.R;
+import de.schildbach.wallet.R;
 
 /**
  * @author Andreas Schildbach
